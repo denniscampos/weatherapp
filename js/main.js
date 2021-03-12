7aade8e0d1e24dfcd7ccc1bad6ca20bb1f02d4a0
Dennis Campos <dennisjr89@gmail.com>
2021-03-11 20:48:12 -0500

    Adjust HTML. Added new HTML section. Styled the new section. Added more functionality on JS for the main container

--- js/main.js
+++ js/main.js
@@ -3,24 +3,38 @@ document.querySelector('button').addEventListener('click', weatherApp)
 
 function weatherApp() {
 
-let location = document.querySelector('input').value    
-let apiKey = "5e7a7eade1f8481381997a34477bd35c"
-let url = "https://api.weatherbit.io/v2.0/forecast/daily?city="+location +"&units=I" +"&key="+apiKey
+const location = document.querySelector('input').value    
+const apiKey = "5e7a7eade1f8481381997a34477bd35c"
+const url = "https://api.weatherbit.io/v2.0/forecast/daily?city="+location +"&units=I" +"&key="+apiKey
 
 fetch(url) 
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        // // // document.querySelector('h2').innerText = data.main.temp
 
-        // //City, State, Country or Zip code
-        // document.querySelector('#city_print').innerText = data.city_name;
+        // Day Variables
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        const getDate = new Date(data.data[0].datetime)
+        const month = getDate.getUTCMonth() + 1 // to get 12 months
+        const date = getDate.getUTCDate()
+        const year = getDate.getUTCFullYear()
+        const day = getDate.getDay()
+
+        document.querySelector(".todays_day").innerText = days[day]
+        document.querySelector(".date_format").innerText = month + "/"+ date + "/" + year
+        document.querySelector(".get_location").innerText = data.city_name
 
         // Current temperature
         document.querySelector('.current_temp').innerText = Math.round(data.data[0].temp) + " F"
 
         // Feels like -- create an average from max and minx
         document.querySelector('.print').innerText = Math.round(data.data[0].app_max_temp) + " F"
+
+        // Humidity
+        document.querySelector(".print_humidity").innerText = data.data[0].rh + "%"
+
+        // Wind Speed
+        document.querySelector(".print_wind").innerText = data.data[0].wind_gust_spd + "m/s"
 
         // // Current Temperature Picture
         // const weatherPic = data.data[0].weather.icon
