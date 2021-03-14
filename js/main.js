706bc75ea79000af3ae4f5b7d9594b59dc3785dc
Dennis Campos <dennisjr89@gmail.com>
2021-03-14 18:19:49 -0400

    adjusted the array for days since Sunday was undefined

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 document.querySelector("button").addEventListener("click", weatherApp);
 
+const apiKey = "5e7a7eade1f8481381997a34477bd35c";
+
 function weatherApp() {
-  const location = document.querySelector("input").value;
-  const apiKey = "5e7a7eade1f8481381997a34477bd35c";
+  const location = document.querySelector("input").value.toLowerCase();
+  
   const url =
     "https://api.weatherbit.io/v2.0/forecast/daily?city=" +
+    location +
+    "&units=I" +
+    "&key=" +
+    apiKey;
+  const url2 =
+    "https://api.weatherbit.io/v2.0/forecast/daily?postal_code=" +
     location +
     "&units=I" +
     "&key=" +
@@ -13,18 +21,16 @@ function weatherApp() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-
       // Day Variables
 
       // prettier-ignore
-      const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"];
       const getDate = new Date(data.data[0].datetime);
       const month = getDate.getUTCMonth() + 1; // to get 12 months
       const date = getDate.getUTCDate();
       const year = getDate.getUTCFullYear();
-      const day = getDate.getDay() + 1;
-      const daysNum = new Date(getDate).getDay() + 1;
+      const day = getDate.getDay();
+      // const daysNum = new Date(getDate).getDay() + 1;
 
       document.querySelector(".todays_day").innerText = days[day];
       document.querySelector(".date_format").innerText =
@@ -37,7 +43,7 @@ function weatherApp() {
 
       // Feels like -- create an average from max and min
       document.querySelector(".print").innerText =
-        Math.round(data.data[0].app_max_temp) + " F"
+        Math.round(data.data[0].app_max_temp) + " F";
 
       // Humidity
       document.querySelector(".print_humidity").innerText =
@@ -50,28 +56,36 @@ function weatherApp() {
       // WEATHER FORECAST
       const getUl = document.querySelector("ul");
       for (let i = 0; i < data.data.length; i++) {
-        const printDay = new Date(data.data[i].datetime)
-        const workPls = printDay.getDay()
+        const printDay = new Date(data.data[i].datetime);
+        const workPls = printDay.getDay();
         getUl.innerHTML += `<li class="weather_print">
                             <div class="weather_contents">
-                                <img src=https://www.weatherbit.io/static/img/icons/${data.data[i].weather.icon}.png alt="" class="forecast_img" />
-                                    <span class="weather_temp">${Math.round(data.data[i].max_temp) + "&#8457;"}</span>
+                                <img src=https://www.weatherbit.io/static/img/icons/${
+                                  data.data[i].weather.icon
+                                }.png alt="" class="forecast_img" />
+                                    <span class="weather_temp">${
+                                      Math.round(data.data[i].max_temp) +
+                                      "&#8457;"
+                                    }</span>
                             </div>
                             <div class="weather_float">
-                                <span class="weather_time">${days[workPls]}</span>
-                                <span class="weather_day">${data.data[i].datetime}</span>
+                                <span class="weather_time">${
+                                  days[workPls]
+                                }</span>
+                                <span class="weather_day">${
+                                  data.data[i].datetime
+                                }</span>
                             </div>
                             </li>
                             </ul>`;
-        
-        console.log(days[workPls]);
       }
-
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
 }
+
+
 
 // const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
@@ -92,13 +106,12 @@ function weatherApp() {
 //  *
 //  */
 
-
-      //   const weatherInfo = document.querySelector("ul");
-      //   for (let i = 0; i < data.data.length; i++) {
-      //     const getDay = document.createElement("li");
-      //     const getImg = document.createElement("img");
-      //     getImg.className = "weather_print";
-      //     getImg.src = `https://www.weatherbit.io/static/img/icons/${data.data[i].weather.icon}.png`;
-      //     getDay.append(getImg);
-      //     weatherInfo.append(getDay);
-      //   }
+//   const weatherInfo = document.querySelector("ul");
+//   for (let i = 0; i < data.data.length; i++) {
+//     const getDay = document.createElement("li");
+//     const getImg = document.createElement("img");
+//     getImg.className = "weather_print";
+//     getImg.src = `https://www.weatherbit.io/static/img/icons/${data.data[i].weather.icon}.png`;
+//     getDay.append(getImg);
+//     weatherInfo.append(getDay);
+//   }
